756bec3bcafad5cdffd2ee41ed52ccfc161559d9
Chris Camp <cpcamp2@gmail.com>
2018-07-24 12:07:24 -0400

    add browser router and some dummy components

--- src/app.js
+++ src/app.js
@@ -4,5 +4,27 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashBoardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
 
-ReactDOM.render(<p>expensify</p>, document.getElementById('app'));
+const AddExpensePage = () => (
+  <div>
+    This is from my add expense component
+  </div>
+);
+
+const routes = (
+  // BrowserRouter expects the child to have a length of 1 so a div must be created
+  // if there are to be multiple routes
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashBoardPage} />
+      <Route path="/create" component={AddExpensePage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
