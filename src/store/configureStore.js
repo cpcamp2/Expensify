3e93b554d72b18a176567c7e9ee57a6383171098
Chris Camp <cpcamp2@gmail.com>
2018-07-26 17:13:00 -0400

    import createStore & combineReducers from redux in conf store file. Import expenses and filters reducers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,8 @@
-
+import {  createStore, combineReducers } from 'redux';
+import expensesReducer from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
 
 // Store creation
-
 const store = createStore(
   combineReducers({
     expenses: expensesReducer,
