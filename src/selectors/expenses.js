5c1a1502e4878ae7fbafb37c18b1a25bc725fbac
Chris Camp <cpcamp2@gmail.com>
2018-07-26 17:09:16 -0400

    export expenses selector

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,5 +1,5 @@
 // Get visible expenses
-const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+export const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
   return expenses.filter((expense) => {
     const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
     const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
