d2d5637c6cfe839849cb86c332f4952bb2d1ee75
Chris Camp <cpcamp2@gmail.com>
2018-07-27 13:34:22 -0400

    create expenseform component with form and import expenseform into addexpensepage

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class ExpenseForm extends React.Component {
+  state = {
+    description: ''
+  };
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Description"
+            autoFocus
+          />
+          <input
+            type="number"
+            placeholder="Amount"
+          />
+          <textarea
+            placeholder="Add a note for your expense (optional)"
+          >
+          </textarea>
+          <button>Add Expense</button>
+        </form>
+      </div>
+    )
+  }
+};
+
+export default ExpenseForm;
