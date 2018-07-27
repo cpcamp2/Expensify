531bd97b7b8ef5eb0a61b38bf253572a2c546ee7
Chris Camp <cpcamp2@gmail.com>
2018-07-27 16:42:41 -0400

    add conditional logic to populate expenseform state to have expense data from edit expense page

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -6,12 +6,12 @@ import 'react-dates/lib/css/_datepicker.css';
 class ExpenseForm extends React.Component {
   constructor(props) {
     super();
-    
-    state = {
-      description: '',
-      note: '',
-      amount: '',
-      createdAt: moment(),
+
+    this.state = {
+      description: props.expense ? props.expense.description : '',
+      note: props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
       calendarFocused: false,
       error: ''
     };
