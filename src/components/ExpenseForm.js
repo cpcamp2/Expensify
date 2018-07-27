cdd9b7194088bf8190f4f338b4d7309479d9e4f5
Chris Camp <cpcamp2@gmail.com>
2018-07-27 13:35:25 -0400

    add functionality to note input in form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,12 +3,18 @@ import React from 'react';
 class ExpenseForm extends React.Component {
   state = {
     description: '',
+    note: ''
   };
 
   onDescriptionChange = (e) => {
     const description = e.target.value;
     this.setState(() => ({ description }));
   };
+
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => ({ note }));
+  }
 
   render() {
     return (
@@ -22,11 +28,13 @@ class ExpenseForm extends React.Component {
             onChange={this.onDescriptionChange}
           />
           <input
-            type="number"
+            type="text"
             placeholder="Amount"
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
+            value={this.state.note}
+            onChange={this.onNoteChange}
           >
           </textarea>
           <button>Add Expense</button>
