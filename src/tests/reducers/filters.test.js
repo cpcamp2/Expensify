85956df355e0cad9e57df978cad0ac6692231649
Chris Camp <cpcamp2@gmail.com>
2018-07-31 18:47:12 -0400

    add test case to test set sortBy date in filters reducer test file

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -15,3 +15,15 @@ test('should set sortBy to amount', () => {
   const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT' });
   expect(state.sortBy).toBe('amount');
 });
+
+test('should set sortBy to date', () => {
+  const currentState = {
+    text: '',
+    sortBy: 'amount',
+    startDate: undefined,
+    endDate: undefined
+  };
+  const action = { type: 'SORT_BY_DATE'};
+  const state = filtersReducer(currentState, action);
+  expect(state.sortBy).toBe('date');
+});
