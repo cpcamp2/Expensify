e4de25f9de20dc3e7f5fd502af9e357f2af9a211
Chris Camp <cpcamp2@gmail.com>
2018-07-31 19:39:34 -0400

    import fixtures/expenses dummy data into reducers/expenses test file and refactor test case remove expense by id

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -6,51 +6,60 @@ test('should set default expenses values', () => {
   expect(state).toEqual([]);
 });
 
-test('should add an expense to state', () => {
-  const expense = {
-    description: '',
-    note: '',
-    amount: 0,
-    createdAt: 0
-  };
-  const action = {
-    type: 'ADD_EXPENSE',
-    expense
-  };
-  const state = expensesReducer(undefined, action);
-  expect(state.length).toBe(1);
-});
-
-test('should remove an expense from state', () => {
-  const currentState = [{
-    id: '123abc',
-    description: '',
-    note: '',
-    amount: 0,
-    createdAt: 0
-  }];
+test('should remove expense by id', () => {
   const action = {
     type: 'REMOVE_EXPENSE',
-    id: '123abc'
+    id: expenses[1].id
   };
-  const state = expensesReducer(currentState, action);
-  expect(state).toEqual([]);
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
 });
 
-test('should edit an expense from state', () => {
-  const currentState = [{
-    id: '123abc',
-    description: '',
-    note: '',
-    amount: 0,
-    createdAt: 0
-  }];
-  const updates = { note: 'New note value' };
-  const action = {
-    type: 'EDIT_EXPENSE',
-    id: '123abc',
-    updates
-  };
-  const state = expensesReducer(currentState, action);
-  expect(state[0].note).toEqual('New note value');
-});
+// test('should add an expense to state', () => {
+//   const expense = {
+//     description: '',
+//     note: '',
+//     amount: 0,
+//     createdAt: 0
+//   };
+//   const action = {
+//     type: 'ADD_EXPENSE',
+//     expense
+//   };
+//   const state = expensesReducer(undefined, action);
+//   expect(state.length).toBe(1);
+// });
+//
+// test('should remove an expense from state', () => {
+//   const currentState = [{
+//     id: '123abc',
+//     description: '',
+//     note: '',
+//     amount: 0,
+//     createdAt: 0
+//   }];
+//   const action = {
+//     type: 'REMOVE_EXPENSE',
+//     id: '123abc'
+//   };
+//   const state = expensesReducer(currentState, action);
+//   expect(state).toEqual([]);
+// });
+//
+// test('should edit an expense from state', () => {
+//   const currentState = [{
+//     id: '123abc',
+//     description: '',
+//     note: '',
+//     amount: 0,
+//     createdAt: 0
+//   }];
+//   const updates = { note: 'New note value' };
+//   const action = {
+//     type: 'EDIT_EXPENSE',
+//     id: '123abc',
+//     updates
+//   };
+//   const state = expensesReducer(currentState, action);
+//   expect(state[0].note).toEqual('New note value');
+// });
