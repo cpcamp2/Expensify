bc66badf0a93ccc83287bff25b87c86091ccfca7
Chris Camp <cpcamp2@gmail.com>
2018-07-31 18:59:38 -0400

    add test case to test set text filter in filters reducer test file

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -11,9 +11,13 @@ test('should setup default filter values', () => {
   });
 });
 
-test('should set sortBy to amount', () => {
-  const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT' });
-  expect(state.sortBy).toBe('amount');
+test('should set text filter', () => {
+  const action = {
+    type: 'SET_TEXT_FILTER',
+    text: 'test'
+  };
+  const state = filtersReducer(undefined, action);
+  expect(state.text).toBe('test');
 });
 
 test('should set sortBy to date', () => {
@@ -23,7 +27,12 @@ test('should set sortBy to date', () => {
     startDate: undefined,
     endDate: undefined
   };
-  const action = { type: 'SORT_BY_DATE'};
+  const action = { type: 'SORT_BY_DATE' };
   const state = filtersReducer(currentState, action);
   expect(state.sortBy).toBe('date');
+});
+
+test('should set sortBy to amount', () => {
+  const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT' });
+  expect(state.sortBy).toBe('amount');
 });
