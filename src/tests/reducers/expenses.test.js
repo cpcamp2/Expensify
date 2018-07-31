da54120b2d8fd2b63f01491493028bf303e69bcd
Chris Camp <cpcamp2@gmail.com>
2018-07-31 19:30:14 -0400

    add test case to reducers/expenses test file that tests that the reducer's default state

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,1 +1,6 @@
 import expensesReducer from '../../reducers/expenses';
+
+test('should set default expenses values', () => {
+  const state = expensesReducer(undefined, { type: '@@INIT' });
+  expect(state).toEqual([]);
+});
