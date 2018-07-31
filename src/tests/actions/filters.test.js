4f17fc029ae89d8e81a0eef658815c5433485f7f
Chris Camp <cpcamp2@gmail.com>
2018-07-31 16:50:37 -0400

    add sort by date test case in actions/filters test file

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -17,6 +17,17 @@ test('should generate set text filter action object with default values', () => 
   });
 });
 
+test('should generate sort by date action object', () => {
+  const action = sortByDate();
+  expect(action).toEqual({
+    type: 'SORT_BY_DATE'
+  });
+});
+
+// test('should generate sort by amount action object', () => {
+//
+// });
+
 test('should generate set start date action object', () => {
   const action = setStartDate(moment(0));
   expect(action).toEqual({
