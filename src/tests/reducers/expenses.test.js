88716ef704a1c685891e84e8e4351afd7963b50f
Chris Camp <cpcamp2@gmail.com>
2018-07-31 19:31:06 -0400

    add test case to reducers/expenses test file that tests that the adding an expense to the state

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -4,3 +4,18 @@ test('should set default expenses values', () => {
   const state = expensesReducer(undefined, { type: '@@INIT' });
   expect(state).toEqual([]);
 });
+
+test('should add an expense to state', () => {
+  const expense = {
+    description: '',
+    note: '',
+    amount: 0,
+    createdAt: 0
+  };
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense
+  };
+  const state = expensesReducer(undefined, action);
+  expect(state.length).toBe(1);
+});
