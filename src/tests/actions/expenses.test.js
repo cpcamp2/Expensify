f287c069047ab421e842e80c23ddfe4d1611a9c2
Chris Camp <cpcamp2@gmail.com>
2018-07-31 16:38:03 -0400

    create filters test file in tests/actions and import filter action generators to file

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,24 +1,5 @@
 import { addExpense, editExpense, removeExpense} from '../../actions/expenses'
 
-test('should setup remove expense action object', () => {
-  const action = removeExpense({ id: '123abc'});
-  expect(action).toEqual({
-    type: 'REMOVE_EXPENSE',
-    id: '123abc'
-  });
-});
-
-test('should setup edit expense action object', () => {
-  const action = editExpense('123abc', {note: 'New note value'});
-  expect(action).toEqual({
-    type: 'EDIT_EXPENSE',
-    id: '123abc',
-    updates: {
-      note: 'New note value'
-    }
-  });
-});
-
 test('should setup add expense action object with provided values', () => {
   const expenseData = {
     description: 'Rent',
@@ -46,6 +27,25 @@ test('should setup add expense action object with default values', () => {
       note: '',
       amount: 0,
       createdAt: 0
+    }
+  });
+});
+
+test('should setup remove expense action object', () => {
+  const action = removeExpense({ id: '123abc'});
+  expect(action).toEqual({
+    type: 'REMOVE_EXPENSE',
+    id: '123abc'
+  });
+});
+
+test('should setup edit expense action object', () => {
+  const action = editExpense('123abc', {note: 'New note value'});
+  expect(action).toEqual({
+    type: 'EDIT_EXPENSE',
+    id: '123abc',
+    updates: {
+      note: 'New note value'
     }
   });
 });
