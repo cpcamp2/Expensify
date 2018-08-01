a15c7ea7ff46b48e742fbe111944be03c9b35a39
Chris Camp <cpcamp2@gmail.com>
2018-08-01 18:26:42 -0400

    add test case to EditExpensePage component test file that tests for handle edit expense

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -14,10 +14,17 @@ beforeEach(() => {
       editExpense={editExpense}
       removeExpense={removeExpense}
       history={history}
+      expense={expenses[2]}
     />
   );
 });
 
 test('should render EditExpensePage correctly', () => {
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle editExpense', () => {
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(editExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
 });
