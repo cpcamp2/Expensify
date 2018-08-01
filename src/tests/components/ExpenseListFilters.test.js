b6425ff57e7be6dbe5d70d45d36df6cef0c50229
Chris Camp <cpcamp2@gmail.com>
2018-08-01 19:40:19 -0400

    import filters and altFilters from fixtures/filters to ExpenseListFilters component test file

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
+import { filters, altFilters } from '../fixtures/filters';
 
 test('', () => {
 
