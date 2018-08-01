f33fbf3a8da93e80e59e34fcf1c9e8fd8c5bae4b
Chris Camp <cpcamp2@gmail.com>
2018-08-01 14:47:38 -0400

    create test case that tests that expenselist renders properly with dummy expenses

--- src/tests/components/ExpenseList.test.js
+++ src/tests/components/ExpenseList.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseList } from '../../components/ExpenseList';
 import expenses from '../fixtures/expenses.js';
+
+test('should render ExpenseList with expenses', () => {
+  const wrapper = shallow(<ExpenseList expenses={expenses}/>);
+  expect(wrapper).toMatchSnapshot();
+});
