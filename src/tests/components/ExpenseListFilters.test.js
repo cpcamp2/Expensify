4e0a18e434bb95fe4cda59a784020e24dfd5a95a
Chris Camp <cpcamp2@gmail.com>
2018-08-01 19:37:05 -0400

    create filters fixture file and import moment

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
+
+test('', () => {
+
+});
+
+// test('', () => {
+//
+// });
