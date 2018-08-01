06f6b86c34529f69756a157fd602ac2edaab06c6
Chris Camp <cpcamp2@gmail.com>
2018-08-01 19:43:34 -0400

    add beforeEach with spies and wrapper so test cases are written more efficiently in ExpenseListFilters component test file

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -3,6 +3,25 @@ import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
 
+let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper
+
+beforeEach(() => {
+  setTextFilter = jest.fn();
+  sortByDate = jest.fn();
+  sortByAmount = jest.fn();
+  setStartDate = jest.fn();
+  setEndDate = jest.fn();
+  wrapper = swallow(
+    <ExpenseListFilters
+      filters={filters}
+      setTextFilter={setTextFilter}
+      sortByDate={sortByDate}
+      sortByAmount={sortByAmount}
+      setStartDate={setStartDate}
+      setEndDate={setEndDate}
+    />)
+});
+
 test('', () => {
 
 });
