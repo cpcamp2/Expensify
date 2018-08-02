10937a3c2a14d92e39c1189c707d697ead93af8d
Chris Camp <cpcamp2@gmail.com>
2018-08-02 13:19:16 -0400

    add conditional logic to see if build is production level and to determine source-maps accordingly

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
-module.exports = () => {
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
   return {
     entry: './src/app.js',
     output: {
@@ -21,7 +23,7 @@ module.exports = () => {
         ]
       }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
