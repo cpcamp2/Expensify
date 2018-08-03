ac711dc3f63c974ce6d9b723766af715d6644c9b
Chris Camp <cpcamp2@gmail.com>
2018-08-03 17:01:18 -0400

    add test case that tests that ExpensesSummary component correctly renders without expenses in ExpensesSummary test file

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -3,7 +3,12 @@ import { shallow } from 'enzyme';
 import { ExpensesSummary } from '../../components/ExpensesSummary';
 import expenses from '../fixtures/expenses';
 
-test('should correctly render ExpensesSummary component', () => {
+test('should correctly render ExpensesSummary component with expenses', () => {
   const wrapper = shallow(<ExpensesSummary expenses={expenses}/>);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should correctly render ExpensesSummary component without expenses', () => {
+  const wrapper = shallow(<ExpensesSummary expenses={[]}/>);
   expect(wrapper).toMatchSnapshot();
 });
