9ec549e23c999f9deb3beea40f14a99a0447119d
Chris Camp <cpcamp2@gmail.com>
2018-08-03 17:18:20 -0400

    add conditional logic to expenses summary for if it is a single expense or multiple expenses, change test cases to test rendering of component for single expense vs multiple

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,14 +3,18 @@ import { connect } from 'react-redux';
 import getExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-export const ExpensesSummary = (props) => (
-  <div>
-    <p>
-      Viewing {props.expenses.length} expenses
-      totaling {numeral((getExpensesTotal(props.expenses)) / 100).format('$0,0.00')}
-    </p>
-  </div>
-);
+export const ExpensesSummary = (props) => {
+  const expenseWord = props.expenses.length === 1 ? 'expense' : 'expenses';
+
+  return (
+    <div>
+      <h4>
+        Viewing {props.expenses.length} {expenseWord} totaling {numeral((getExpensesTotal(props.expenses)) / 100).format('$0,0.00')}
+      </h4>
+    </div>
+  );
+};
+
 
 const mapStateToProps = (state) => ({ expenses: state.expenses });
 
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -3,12 +3,12 @@ import { shallow } from 'enzyme';
 import { ExpensesSummary } from '../../components/ExpensesSummary';
 import expenses from '../fixtures/expenses';
 
-test('should correctly render ExpensesSummary component with expenses', () => {
-  const wrapper = shallow(<ExpensesSummary expenses={expenses}/>);
+test('should correctly render ExpensesSummary component with single expense', () => {
+  const wrapper = shallow(<ExpensesSummary expenses={[expenses[0]]}/>);
   expect(wrapper).toMatchSnapshot();
 });
 
-test('should correctly render ExpensesSummary component without expenses', () => {
-  const wrapper = shallow(<ExpensesSummary expenses={[]}/>);
+test('should correctly render ExpensesSummary component with multiple expenses', () => {
+  const wrapper = shallow(<ExpensesSummary expenses={expenses}/>);
   expect(wrapper).toMatchSnapshot();
 });
