7aa9d697fec366c8a9e2d91e6c47c9d1df2d9e79
Chris Camp <cpcamp2@gmail.com>
2018-08-07 20:34:35 -0400

    add code playing with firebase as a reference for fetching data

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,38 +13,71 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: 'Penn Camp',
-  age: 25,
-  stressLife: 6,
-  job: {
-    title: 'Software Developer',
-    company: 'Google'
-  },
-  location: {
-    city: 'New York',
-    country: 'United States'
-  }
-}).then(() => {
-  console.log('Data is saved');
-}).catch((error) => {
-  console.log('This failed.', error);
-});
+database.ref().on('value', (snapshot) => {
+  const val = snapshot.val();
+  console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+})
 
-database.ref().update({
-  stressLife: 9,
-  'job/company': 'Amazon',
-  'location/city': 'Seattle'
-}).then(() => {
-  console.log('Update successful')
-}).catch((error) => {
-  console.log('Update failed', error)
-});
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   console.log(snapshot.val());
+// }, (error) =>{
+//   console.log('Error with data fetching', error);
+// });
+//
+// setTimeout(() => {
+//   database.ref('age').set(29);
+// }, 3500);
+//
+// setTimeout(() => {
+//   database.ref().off(onValueChange);
+// }, 7000);
+//
+// setTimeout(() => {
+//   database.ref('age').set(30);
+// }, 10500);
 
-database.ref()
-  .remove()
-  .then(() => {
-    console.log('Data was removed');
-  }).catch((error) => {
-    console.log('Data removal failed.', error);
-  });
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((error) => {
+//     console.log('Error fetching data', error);
+//   });
+
+// database.ref().set({
+//   name: 'Penn Camp',
+//   age: 25,
+//   stressLife: 6,
+//   job: {
+//     title: 'Software Developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'New York',
+//     country: 'United States'
+//   }
+// }).then(() => {
+//   console.log('Data is saved');
+// }).catch((error) => {
+//   console.log('This failed.', error);
+// });
+
+// database.ref().update({
+//   stressLife: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// }).then(() => {
+//   console.log('Update successful')
+// }).catch((error) => {
+//   console.log('Update failed', error)
+// });
+
+// database.ref()
+//   .remove()
+//   .then(() => {
+//     console.log('Data was removed');
+//   }).catch((error) => {
+//     console.log('Data removal failed.', error);
+//   });
